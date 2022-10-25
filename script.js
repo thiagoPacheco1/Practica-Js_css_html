c2105138ba894f956d85251f3bd6971f53240353
Thiago Pacheco <ingthiagopacheco@hotmail.com>
2022-10-25 20:38:31 -0300

    Navar Desktop, menu movil  integrado

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menumovil = document.querySelector('.menu-container');
+const MenuMovil = document.querySelector('.menu');
+
+
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
-
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+
+MenuMovil.addEventListener('click',togglemenumovil);
+function togglemenumovil() {
+    menumovil.classList.toggle('inactive');
 }
