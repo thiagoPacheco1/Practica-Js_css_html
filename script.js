d064af3efd64cb3583a072bf316a8645018f7d10
Thiago Pacheco <ingthiagopacheco@hotmail.com>
2022-10-25 23:51:48 -0300

    Navar Desktop, menu movil, carrito  integrado

--- script.js
+++ script.js
@@ -4,16 +4,46 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menumovil = document.querySelector('.menu-container');
 const MenuMovil = document.querySelector('.menu');
 
+const carrito = document.querySelector('.carrito');
+const titlee= document.querySelector('.titleg');
+const toggcarrito = document.querySelector('.product-detail');
+
 
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
-}
+    if (!desktopMenu.classList.contains('inactive')) {
+        toggcarrito.classList.add('inactive')
+    }
 
+   
+
+}
 
 MenuMovil.addEventListener('click',togglemenumovil);
 function togglemenumovil() {
     menumovil.classList.toggle('inactive');
+if (!menumovil.classList.contains('inactive')) {
+    toggcarrito.classList.add('inactive');
 }
+
+
+}
+titlee.addEventListener('click',togcarrito);
+carrito.addEventListener('click',togcarrito);
+function togcarrito() {
+    toggcarrito.classList.toggle('inactive');
+
+    if (!toggcarrito.classList.contains('inactive')) {
+        menumovil.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+        
+    }  
+
+   
+
+
+}
+
